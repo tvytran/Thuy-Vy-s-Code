714e77dfc5cec531d19a823667be08f697a7ea14
tvytran <110182087+tvytran@users.noreply.github.com>
2023-02-14 14:04:47 -0500

    Update Version2WithTime.java

--- Version2WithTime.java
+++ Version2WithTime.java
@@ -54,7 +54,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Robot: Auto Drive By Time", group="Robot")
+@Autonomous(name= "VERSION2WITHTIME", group="Robot")
 @Disabled
 public class Version2WithTime extends LinearOpMode {
 
